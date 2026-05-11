be6346282c15bf0f0f41af7d03f7f6a5772bb811
JuuJap <juliobarcelos0104@gmail.com>
2026-05-11 17:09:56 -0300

    Atualização de dados

--- ControlePonto/db.php
+++ ControlePonto/db.php
@@ -10,5 +10,8 @@ $conexao = new mysqli($host, $usuario, $senha, $banco);
 if($conexao->connect_error){
     die("Erro: " . $conexao->connect_error);
 }
-
+/*
+TODO O CONTEUDO CRIADO AQUI FOI DESENVOLVIDO PELOS ESTUDANTES JULIO APARECIDO, MATHEUS BASSI E JULIO CESAR, 
+SO INDEVIDO SERA CONSIDERADA COMO PLAGIO, E VAMOS ENCARAR COMO PURA BABAQUICE DA SUA PARTE
+  */
 ?>
