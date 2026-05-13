3107f7c81688add146851cd8d4a84376e043092c
JuuJap <juliobarcelos0104@gmail.com>
2026-05-12 21:23:45 -0300

    Update db.php

--- ControlePonto/db.php
+++ ControlePonto/db.php
@@ -1,17 +1,20 @@
 <?php
 
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "controle_ponto";
+$host = $_ENV['MYSQLHOST'];
+$usuario = $_ENV['MYSQLUSER'];
+$senha = $_ENV['MYSQLPASSWORD'];
+$banco = $_ENV['MYSQLDATABASE'];
 
 $conexao = new mysqli($host, $usuario, $senha, $banco);
 
-if($conexao->connect_error){
+if ($conexao->connect_error) {
     die("Erro: " . $conexao->connect_error);
 }
+
+$conexao->set_charset("utf8");
+
 /*
-TODO O CONTEUDO CRIADO AQUI FOI DESENVOLVIDO PELOS ESTUDANTES JULIO APARECIDO, MATHEUS BASSI E JULIO CESAR, 
+TODO O CONTEUDO CRIADO AQUI FOI DESENVOLVIDO PELOS ESTUDANTES JULIO APARECIDO, MATHEUS BASSI E JULIO CESAR,
 USO INDEVIDO SERA CONSIDERADA COMO PLAGIO, E VAMOS ENCARAR COMO PURA BABAQUICE DA SUA PARTE
-  */
+*/
 ?>
