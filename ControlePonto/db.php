439e2dc0bff8b044ae88f30760adf3f7d632af3e
JuuJap <juliobarcelos0104@gmail.com>
2026-05-12 21:32:06 -0300

    Update db.php

--- ControlePonto/db.php
+++ ControlePonto/db.php
@@ -1,20 +1,25 @@
 <?php
 
-$host = $_ENV['MYSQLHOST'];
-$usuario = $_ENV['MYSQLUSER'];
-$senha = $_ENV['MYSQLPASSWORD'];
-$banco = $_ENV['MYSQLDATABASE'];
+$host = getenv('MYSQLHOST');
+$usuario = getenv('MYSQLUSER');
+$senha = getenv('MYSQLPASSWORD');
+$banco = getenv('MYSQLDATABASE');
+$porta = getenv('MYSQLPORT');
 
-$conexao = new mysqli($host, $usuario, $senha, $banco);
+$conexao = new mysqli(
+    $host,
+    $usuario,
+    $senha,
+    $banco,
+    $porta
+);
 
 if ($conexao->connect_error) {
-    die("Erro: " . $conexao->connect_error);
+    die("Erro na conexão: " . $conexao->connect_error);
 }
 
 $conexao->set_charset("utf8");
 
-/*
-TODO O CONTEUDO CRIADO AQUI FOI DESENVOLVIDO PELOS ESTUDANTES JULIO APARECIDO, MATHEUS BASSI E JULIO CESAR,
-USO INDEVIDO SERA CONSIDERADA COMO PLAGIO, E VAMOS ENCARAR COMO PURA BABAQUICE DA SUA PARTE
-*/
+//TODO O CONTEUDO CRIADO AQUI FOI DESENVOLVIDO PELOS ESTUDANTES JULIO APARECIDO, MATHEUS BASSI E JULIO CESAR,//
+//USO INDEVIDO SERA CONSIDERADA COMO PLAGIO, E VAMOS ENCARAR COMO PURA BABAQUICE DA SUA PARTE//
 ?>
