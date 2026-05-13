208f5f72ac059d6a751045403a0fc397d5613d32
JuuJap <juliobarcelos0104@gmail.com>
2026-05-12 22:06:52 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = getenv('MYSQLHOST');
 $usuario = getenv('MYSQLUSER');
 $senha = getenv('MYSQLPASSWORD');
@@ -15,11 +18,12 @@ $conexao = new mysqli(
 );
 
 if ($conexao->connect_error) {
-    die("Erro: " . $conexao->connect_error);
+    die("Erro na conexão: " . $conexao->connect_error);
 }
 
 $conexao->set_charset("utf8");
 
 //TODO O CONTEUDO CRIADO AQUI FOI DESENVOLVIDO PELOS ESTUDANTES JULIO APARECIDO, MATHEUS BASSI E JULIO CESAR,//
 //USO INDEVIDO SERA CONSIDERADA COMO PLAGIO, E VAMOS ENCARAR COMO PURA BABAQUICE DA SUA PARTE//
+
 ?>
